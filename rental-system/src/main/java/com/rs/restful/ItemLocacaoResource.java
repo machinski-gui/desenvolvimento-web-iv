361ee4833e6dc880c0f43465628550d3231607a4
Guilherme <Guilherme@DESKTOP-4TBES0S>
2019-12-05 17:11:04 -0300

    Locação Finalizada

--- rental-system/src/main/java/com/rs/restful/ItemLocacaoResource.java
+++ rental-system/src/main/java/com/rs/restful/ItemLocacaoResource.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rs.entity.ItemLocacao;
+import com.rs.entity.Locacao;
 import com.rs.service.ItemLocacaoService;
 
 @Component
@@ -20,10 +23,20 @@ public class ItemLocacaoResource {
 	@Autowired
 	private ItemLocacaoService itemLocacaoService;
 	
+	@PostMapping( "/insert" )
+	public ItemLocacao cadastrar( @RequestBody ItemLocacao itemLocacao ) {
+		return this.itemLocacaoService.cadastrarItemLocacao(itemLocacao);
+	}
+	
 	@GetMapping("/list")
 	public List<ItemLocacao> listar() {
 		return this.itemLocacaoService.listarItemLocacao();
-	}	
+	}
+	
+	@GetMapping("/detail")
+	public List<ItemLocacao> detalhar(@RequestParam("id") Long id) {
+		return this.itemLocacaoService.detalharItemLocacao(id);
+	}
 }
 
 
--- rental-system/src/main/java/com/rs/restful/LocacaoResource.java
+++ rental-system/src/main/java/com/rs/restful/LocacaoResource.java
@@ -1,10 +1,11 @@
 package com.rs.restful;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,14 +27,14 @@ public class LocacaoResource {
 	@Autowired
 	private ItemLocacaoService itemLocacaoService;
 	
+	@PostMapping( "/insert" )
+	public Locacao cadastrar( @RequestBody Locacao locacao ) {
+		return this.locacaoService.cadastrarLocacao(locacao);
+	}
+	
 	@GetMapping("/list")
 	public List<Locacao> listar() {
 		return this.locacaoService.listarLocacoes();
-	}
-	
-	@GetMapping("/detail")
-	public List<ItemLocacao> detalhar(@RequestParam("id") Long id) {
-		return this.itemLocacaoService.detalharItemLocacao(id);
 	}
 }
 
